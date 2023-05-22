fe2a1002675ddf63035ee9a92cbd613a24c78afe
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2023-05-22 18:59:52 +0500

    Add search script

---
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/search.js
@@ -0,0 +1,33 @@
+﻿const ENTER = 13
+
+const searchInput = document.querySelector('#search-input');
+const searchIcon = document.querySelector('#search-icon');
+
+searchIcon.addEventListener('click', event => {
+    search(searchInput.value);
+});
+
+searchInput.addEventListener('keydown', event => {
+    if (event.keyCode === ENTER) {
+        event.preventDefault();
+        search(searchInput.value);
+    }
+});
+
+function search(fanficName) {
+    fetch(`/fanfic?title=${fanficName}`)
+        .then(response => {
+            if (response.ok) {
+                alert("Такой фанфик существует!")
+            } else if (response.status === 404) {
+                alert("Фанфик не существует")
+            } else {
+                alert(response.statusText)
+            }
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
+
+
