7842ce0374324a8be081ecb9bd41d5a9870bd899
AndreyKuklinov <Andreydolphin@mail.ru>
2023-05-27 19:20:49 +0500

    Move modal to script

---
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/modal.js
@@ -0,0 +1,25 @@
+const modal = document.createElement('modal');
+
+modal.innerHTML = `
+    <div class="modal">
+        <div class="modal-content">
+            <img src="/assets/images/cross-icon.svg" class="close-button"/>
+            <h1>Вход</h1>
+            <form>
+                <div class="field">
+                    <label for="email">Электронная почта:</label>
+                    <input type="email" id="email" required/>
+                </div>
+                <div class="field">
+                    <label for="password">Пароль:</label>
+                    <input type="password" id="password" required/>
+                </div>
+                
+                <button>Войти</button>
+                <button>Зарегистрироваться</button>
+            </form>
+        </div>
+    </div>
+`;
+document.body.appendChild(modal);
+
