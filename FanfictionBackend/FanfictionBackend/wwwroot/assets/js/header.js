b9ded1d19fbd38536bef88047194950fe7850743
AndreyKuklinov <Andreydolphin@mail.ru>
2023-05-28 11:22:04 +0500

    Move stuff from modal.js to header.js

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/header.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/header.js
@@ -1,17 +1,49 @@
-﻿const header = document.createElement('header');
+﻿function createLoginModal() {
+    const loginModal = document.createElement('modal');
+    loginModal.innerHTML = `
+        <div class="modal">
+            <div class="modal-content">
+                <img src="/assets/images/cross-icon.svg" class="close-button"/>
+                <h1>Вход</h1>
+                <form>
+                    <div class="field">
+                        <label for="email">Электронная почта:</label>
+                        <input type="email" id="email" required/>
+                    </div>
+                    <div class="field">
+                        <label for="password">Пароль:</label>
+                        <input type="password" id="password" required/>
+                    </div>
+                    
+                    <button>Войти</button>
+                    <button>Зарегистрироваться</button>
+                </form>
+            </div>
+        </div>
+    `;
+    document.body.appendChild(loginModal);
+    return loginModal;
+};
 
-header.innerHTML = `
-  <div class="header-container">
-    <img class="main-page-button" src="/assets/images/logo.svg" alt="Back to main page" />
-    <div class="search-bar">
-      <input id="search-input" type="text" placeholder="Поиск" />
-      <img id="search-icon" src="/assets/images/search_icon.svg" alt="Search" />
+function createHeader() {
+    const header = document.createElement('header');
+    header.innerHTML = `
+    <div class="header-container">
+        <img class="main-page-button" src="/assets/images/logo.svg" alt="Back to main page" />
+        <div class="search-bar">
+        <input id="search-input" type="text" placeholder="Поиск" />
+        <img id="search-icon" src="/assets/images/search_icon.svg" alt="Search" />
+        </div>
+        <img class="profile-button" src="/assets/images/profile.svg" alt="Go to profile" /> 
     </div>
-    <img class="profile-button" src="/assets/images/profile.svg" alt="Go to profile" /> 
-  </div>
-`;
-document.body.appendChild(header);
+    `;
+    document.body.appendChild(header);
+    return header;
+}
 
+
+const header = createHeader();
+const loginModal = createLoginModal();
 
 const searchInput = document.querySelector('#search-input');
 const searchIcon = document.querySelector('#search-icon');
