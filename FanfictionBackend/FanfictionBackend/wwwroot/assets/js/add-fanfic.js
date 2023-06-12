f9e393f04564bb90867b10ab8ede3efa2db39689
Sokolova Anna <anna.sok.03@mail.ru>
2023-06-12 18:35:47 +0500

    +js к addfanfic но беды с отправлением при АддАрт

---
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/add-fanfic.js
@@ -0,0 +1,80 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+const name = document.getElementById('name');
+const fandom = document.getElementById('fandom');
+const persons = document.getElementById('persons');
+const pairings = document.getElementById('pairings');
+const rating = document.getElementById('rating');
+const _focus = document.getElementById('focus');
+const genre = document.getElementById('genre');
+const warning = document.getElementById('warning');
+const parameters = document.getElementById('parameters');
+const translation = document.getElementById('translation');
+const shortDescription = document.getElementById('shortDescription');
+const note = document.getElementById('note');
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+        name: name.value,
+        fandom: fandom.value,
+        persons: persons.value,
+        pairings: pairings.value,
+        rating: rating.value,
+        focus: _focus.value,
+        genre: genre.value,
+        warning: warning.value,
+        parameters: parameters.value,
+        translation: translation.value,
+        shortDescription: shortDescription.value,
+        note: note.value
+    };
+    publishFanfic(data)
+        .catch(() => alert("Не удалось опубликовать фанфик"));
+}
+
+async function publishFanfic(data) {
+    const response = await fetch('/fanfics', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
+
+  function addArt() {
+    const uploadBtn = document.getElementById('uploadBtn');
+  const fileList = document.getElementById('fileList');
+  const uploadedFiles = new Set();
+
+  uploadBtn.addEventListener('click', () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.multiple = true;
+    input.accept = '.png, .jpeg, .jpg';
+    input.addEventListener('change', () => {
+      const files = input.files;
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (!uploadedFiles.has(file.name)) {
+          uploadedFiles.add(file.name);
+          const li = document.createElement('li');
+          li.textContent = file.name;
+          fileList.appendChild(li);
+        }
+      }
+    });
+    input.click();
+  });
+
+  fileList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+      const fileName = event.target.textContent;
+      uploadedFiles.delete(fileName);
+      event.target.remove();
+    }
+  });
+  }
