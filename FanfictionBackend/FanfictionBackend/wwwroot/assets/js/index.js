1b7e80c22c97c64988d2235bce52a8a5f9f7be61
Sokolova Anna <anna.sok.03@mail.ru>
2023-06-17 15:59:48 +0500

    - div в index.html

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/index.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/index.js
@@ -1,35 +1,7 @@
 const fanficsInfo = getFanfics();
-const containers = document.querySelectorAll('.work-container[data-id]');
+addFanficsRow();
 
-containers.forEach(container => {
-  const id = container.dataset.id;
-  const likes = container.querySelector(`#likes`);
-  const name = container.querySelector(`#nameFanfic`);
-  const description = container.querySelector(`#descriptionFanfic`);
-
-  const [nameInfo, likesInfo, descriptionInfo, link] = fanficsInfo[id];
-  //console.log([nameInfo, likesInfo, descriptionInfo]);
-
-  name.setAttribute('style', 'cursor: pointer;');
-
-  if (nameInfo === null || descriptionInfo === null) {
-    container.remove();
-  } else {
-    name.textContent = nameInfo;
-    description.textContent = descriptionInfo;
-  }
-
-  likes.textContent = likesInfo || 0;
-
-  name.setAttribute('data-link', link);
-  name.addEventListener('click', () => {
-    const link = name.getAttribute('data-link');
-    window.location.href = link;
-  });
-});
-
-const authorsBtn = document.querySelector('#authorsBtn');
-authorsBtn.setAttribute('href', 'authors-list.html');
+const authorsBtn = document.getElementById('authorsBtn');
 authorsBtn.addEventListener('click', () => {
   window.location.href = 'authors-list.html';
 });
@@ -38,4 +10,73 @@ function getFanfics() {
     return [['Автомобилисты плачут и платят', '9', 'Это история о том, как бедный студент Василий решил преобрести себе машину, чтобы ездить на пары. Но он не знал, оо! Он не знал, что есть бензин и налоги. А еще кривые дороги!', 'fanfic-page.html'], 
 ['Юра и пельмехи', '999', 'Юра решил сварить пельмехи. Кто знал, что в этот момент в его стене откроется портал в другое измерение и оттуда выпадет жуткое существо? Что ж, теперь ужин не будет таким одиноким', 'fanfic-page.html'],
 ['Автовокзал', '268', 'Люди отправлялись на автовокзал, вывозящий их из города, как к точке спасения, к месту, где они обретут новую жизнь. Но никто из них не знал, что автобусы везут совсем не в райские места', 'fanfic-page.html']];
+}
+
+
+function addFanficsRow() {
+  const profileFanficRow = document.getElementById('profile-fanfics-row');
+
+  for (let j = 0; j < 3; j++) {
+      const info = fanficsInfo[j];
+      if (info[0] == null) continue;
+      const workContainer = createWorkContainer(info, j);
+      profileFanficRow.appendChild(workContainer);
+  }
+}
+
+function createWorkContainer(info, number) {
+  const workContainer = document.createElement('section');
+  workContainer.classList.add('work-container');
+  workContainer.setAttribute('data-id', number)
+
+  //Добавление лайков
+  const likesRow = document.createElement('div');
+  likesRow.classList.add('likes-row');
+
+  const likesContainer = document.createElement('div');
+  likesContainer.classList.add('likes-container');
+
+  const labelLikes = document.createElement('label');
+  labelLikes.textContent = info[1] || 0;
+
+  const starLikesIcon = document.createElement('img');
+  starLikesIcon.classList.add('star_likes_icon');
+  starLikesIcon.setAttribute('src', 'assets/images/star_likes.svg');
+  starLikesIcon.setAttribute('alt', 'star_likes');
+
+  likesContainer.appendChild(labelLikes);
+  likesContainer.appendChild(starLikesIcon);
+
+  likesRow.appendChild(likesContainer);
+
+  workContainer.appendChild(likesRow);
+
+  //Добавление имени фанфика
+  const workNameContainer = document.createElement('div');
+  workNameContainer.classList.add('work-name-container');
+
+  const nameFanfic = document.createElement('h3');
+  nameFanfic.textContent = info[0];
+  nameFanfic.setAttribute('style', 'cursor: pointer;');
+
+  nameFanfic.setAttribute('data-link', info[3]);
+  nameFanfic.addEventListener('click', () => {
+      const link = nameFanfic.getAttribute('data-link');
+      window.location.href = link;
+  });
+
+  workNameContainer.appendChild(nameFanfic);
+  workContainer.appendChild(workNameContainer);
+
+  //Добавление описания фанфика
+  const workDescriptionContainer = document.createElement('div');
+  workDescriptionContainer.classList.add('work-description-container');
+
+  const descriptionFanfic = document.createElement('p');
+  descriptionFanfic.textContent = info[2];
+
+  workDescriptionContainer.appendChild(descriptionFanfic);
+  workContainer.appendChild(workDescriptionContainer);
+
+  return workContainer;
 }
