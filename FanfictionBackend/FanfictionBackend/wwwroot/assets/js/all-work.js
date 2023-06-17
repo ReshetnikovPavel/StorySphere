8857064d4d80e21854a7dc8ebee30fa16b3e5e79
Sokolova Anna <anna.sok.03@mail.ru>
2023-06-17 12:54:52 +0500

    - div в profile.html

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/all-work.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/all-work.js
@@ -1,6 +1,6 @@
 const fanficsInfo = getFanfics();
 
-let dataIndex = 0; // текущий фанфик в массиве
+let j = 0; // текущий фанфик в массиве
 const dataLength = fanficsInfo.length; //всего фф
 const searchResultRowContainer = document.getElementById('search-result-row-container');
 let rowIndex = 0; //строчка фф
@@ -11,31 +11,31 @@ if (dataLength === 0) {
     sorry.textContent = 'Извините, ничего не найдено :( Даже печенек с чаем нет!'
     searchResultRowContainer.appendChild(sorry);
 } else {
-    addSearchResultRow();
-    addSearchResultRow();
-    addSearchResultRow();
+    addFanficsRow();
+    addFanficsRow();
+    addFanficsRow();
 
     window.addEventListener('scroll', function() {
-        addSearchResultRow();
+        addFanficsRow();
     });
 }
 
-function addSearchResultRow() {
+function addFanficsRow() {
     const searchResultRowContainer = document.getElementById('search-result-row-container');
 
     // Создание search-result-row и добавление его в searchResultRowContainer с разными data-id
-    const searchResultRow = document.createElement('div');
+    const searchResultRow = document.createElement('section');
     searchResultRow.classList.add('search-result-row');
     searchResultRow.setAttribute('data-id', rowIndex); // Установка разных data-id
     rowIndex++;
     // Создание и добавление трех work-container в каждый search-result-row
     for (let j = 0; j < 3; j++) {
 
-        const info = fanficsInfo[dataIndex];
+        const info = fanficsInfo[j];
 
-        const workContainer = document.createElement('div');
+        const workContainer = document.createElement('section');
         workContainer.classList.add('work-container');
-        workContainer.setAttribute('data-id', cycle * dataLength + dataIndex)
+        workContainer.setAttribute('data-id', cycle * dataLength + j)
 
         // Добавление других div в каждый work-container
 
@@ -47,7 +47,6 @@ function addSearchResultRow() {
         likesContainer.classList.add('likes-container');
 
         const labelLikes = document.createElement('label');
-        labelLikes.setAttribute('id', 'likes');
         labelLikes.textContent = info[1] || 0;
 
         const starLikesIcon = document.createElement('img');
@@ -67,8 +66,7 @@ function addSearchResultRow() {
         const workNameContainer = document.createElement('div');
         workNameContainer.classList.add('work-name-container');
 
-        const nameFanfic = document.createElement('label');
-        nameFanfic.setAttribute('id', 'nameFanfic');
+        const nameFanfic = document.createElement('h3');
         nameFanfic.textContent = info[0];
         nameFanfic.setAttribute('style', 'cursor: pointer;');
 
@@ -87,15 +85,14 @@ function addSearchResultRow() {
         workDescriptionContainer.classList.add('work-description-container');
 
         const descriptionFanfic = document.createElement('p');
-        descriptionFanfic.setAttribute('id', 'descriptionFanfic');
         descriptionFanfic.textContent = info[2];
 
         workDescriptionContainer.appendChild(descriptionFanfic);
         workContainer.appendChild(workDescriptionContainer);
 
-        dataIndex++;
-        if (dataIndex === dataLength) {
-            dataIndex = 0;
+        j++;
+        if (j === dataLength) {
+            j = 0;
             cycle++;
         }
         // Добавление work-container в search-result-row
