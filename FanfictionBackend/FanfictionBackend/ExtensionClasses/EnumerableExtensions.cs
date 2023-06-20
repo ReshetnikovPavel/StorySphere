8c333c5c82cb9f06b4457f6ef023a1bb30b4111d
Pavel Reshetnikov <pavelresh@yandex.ru>
2023-06-20 16:28:41 +0500

    Changed ToPagedList from IQueriable to IEnumerable

--- FanfictionBackend/FanfictionBackend/ExtensionClasses/EnumerableExtensions.cs
+++ FanfictionBackend/FanfictionBackend/ExtensionClasses/EnumerableExtensions.cs
@@ -2,9 +2,9 @@ using FanfictionBackend.Pagination;
 
 namespace FanfictionBackend.ExtensionClasses;
 
-public static class QueryableExtensions
+public static class EnumerableExtensions
 {
-    public static PagedList<T> ToPagedList<T>(this IQueryable<T> source, PagingParameters pp)
+    public static PagedList<T> ToPagedList<T>(this IEnumerable<T> source, PagingParameters pp)
     {
         var metadata = new PagingMetadata(source.Count(), pp);
         var items = source.Skip((pp.PageNumber - 1) * pp.PageSize).Take(pp.PageSize);
