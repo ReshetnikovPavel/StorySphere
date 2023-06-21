cbe344bb9218f89388960f8ecc493534ba28b5f0
AndreyKuklinov <Andreydolphin@mail.ru>
2023-06-21 18:45:31 +0500

    Implement authentication app definition

--- FanfictionBackend/FanfictionBackend/AppDefinitions/AuthenticationAppDefinition.cs
+++ FanfictionBackend/FanfictionBackend/AppDefinitions/AuthenticationAppDefinition.cs
@@ -1,10 +1,34 @@
-﻿using FanfictionBackend.Interfaces;
+﻿using System.Text;
+using FanfictionBackend.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FanfictionBackend.AppDefinitions;
 
 public class AuthenticationAppDefinition : IAppDefinition
 {
-    public void DefineApp(WebApplication app){ }
+    public void DefineApp(WebApplication app)
+    {
+        app.UseAuthorization();
+        app.UseAuthentication();
+    }
 
-    public void DefineServices(IServiceCollection services, IConfiguration config) { }
+    public void DefineServices(IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateActor = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config["Jwt:Issuer"],
+                ValidAudience = config["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                    config["Jwt:Key"] ?? throw new InvalidOperationException("Security key can't be null")))
+            };
+        });
+        services.AddAuthorization();
+    }
 }
