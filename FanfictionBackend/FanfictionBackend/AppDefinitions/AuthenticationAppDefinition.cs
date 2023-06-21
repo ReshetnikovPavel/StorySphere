178b954ae309822319eb7e9ea7e2a60dcc2d4bc4
AndreyKuklinov <Andreydolphin@mail.ru>
2023-06-21 18:42:16 +0500

    Add empty authentication app def

---
+++ FanfictionBackend/FanfictionBackend/AppDefinitions/AuthenticationAppDefinition.cs
@@ -0,0 +1,10 @@
+﻿using FanfictionBackend.Interfaces;
+
+namespace FanfictionBackend.AppDefinitions;
+
+public class AuthenticationAppDefinition : IAppDefinition
+{
+    public void DefineApp(WebApplication app){ }
+
+    public void DefineServices(IServiceCollection services, IConfiguration config) { }
+}
