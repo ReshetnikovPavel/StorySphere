e4fb30f0304d566cb1ef28c939630d0817a8385a
AndreyKuklinov <Andreydolphin@mail.ru>
2023-06-22 13:55:42 +0500

    Authorize hello world

--- FanfictionBackend/FanfictionBackend/Program.cs
+++ FanfictionBackend/FanfictionBackend/Program.cs
@@ -1,21 +1,97 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using FanfictionBackend.AppDefinitions;
 using FanfictionBackend.ExtensionClasses;
 using FanfictionBackend.Interfaces;
 using FanfictionBackend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.BuildByDefinitions(
-    new FanficAppDefinition(),
-    new SwaggerAppDefinition(),
-    new AuthenticationAppDefinition()
-);
-
-using (var scope = app.Services.CreateScope())
+builder.Services.AddSwaggerGen(options =>
 {
-    var demoFactory = scope.ServiceProvider.GetRequiredService<DemoFactory>();
-    demoFactory.InitData();
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddAuthorization();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapPost("/login", (string username) => Login(username));
+
+app.MapDelete("/hello",
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    () => "Hello, world!");
+
+IResult Login(string username)
+{
+    var claims = new[]
+    {
+        new Claim(ClaimTypes.NameIdentifier, username),
+    };
+
+    var token = new JwtSecurityToken
+    (
+        issuer: builder.Configuration["Jwt:Issuer"],
+        audience: builder.Configuration["Jwt:Audience"],
+        claims: claims,
+        expires: DateTime.UtcNow.AddDays(60),
+        notBefore: DateTime.UtcNow,
+        signingCredentials: new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            SecurityAlgorithms.HmacSha256)
+    );
+
+    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+    return Results.Ok(tokenString);
 }
 
-app.UseFileServer();
+app.UseSwaggerUI();
+
 app.Run();
