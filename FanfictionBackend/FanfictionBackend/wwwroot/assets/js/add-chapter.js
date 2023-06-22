a55b3f268630b08e5c606e387eb6e8e6c420a26e
SoftOwl256 <anna.sok.03@mail.ru>
2023-06-22 15:44:28 +0500

    РУКОПИСИ ГОРЯТ (- черновики)

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/add-chapter.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/add-chapter.js
@@ -1,27 +1,10 @@
 
-const draftBtn = document.querySelector('#draft');
 const publishBtn = document.querySelector('#publish');
 
 publishBtn.addEventListener("click", publishSubmit);
-draftBtn.addEventListener("click", saveDraftSubmit);
 
 const _name = document.getElementById('name');
 const content = document.getElementById('content');
-
-function saveDraftSubmit(event) {
-    event.preventDefault();
-    if (_name.value.trim() === '') {
-        alert('Введите название главы');
-        return;
-    }
-    
-    const data = {
-        name: _name.value,
-        content: content.value,
-    };
-    saveDraft(data)
-        .catch(() => alert("Не удалось сохранить черновик"));
-}
 
 function publishSubmit(event) {
     event.preventDefault();
@@ -42,17 +25,6 @@ function publishSubmit(event) {
 function getFanficId() {
     const url = new URL(window.location.href);
     return url.searchParams.get("fanficId")
-}
-
-async function saveDraft(data) {
-    const response = await fetch('/drafts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    return await response.json();
 }
 
 async function publish(chapter, fanficId) {
