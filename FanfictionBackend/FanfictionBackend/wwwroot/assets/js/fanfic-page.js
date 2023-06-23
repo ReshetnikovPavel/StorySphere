62e7a4fe0de12c1fe30f6b237e9a550cf62364a7
SoftOwl256 <anna.sok.03@mail.ru>
2023-06-23 11:56:37 +0500

    Вывод из базы в fanfic-page

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/fanfic-page.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/fanfic-page.js
@@ -1,215 +1,278 @@
-let fanficId = 1;
+let fanficId = 2; //getFanficId();
+main();
 
-let fanficNameInfo;
-let authorInfo;
-let ratingInfo;
-let focusInfo;
-let likesInfo;
-// const sizeChaptersInfo = fanfic.numChapter;
-let charactersInfo;
-let pairingsInfo;
-let genresInfo;
-let warningsInfo;
-let descriptionInfo;
-let notesInfo;
-let fandomInfo;
+async function main() {
+    let fanfic;
+    try {
+        fanfic = await fetchFanfic(fanficId);
+    } catch (error) {
+        console.error(`Error fetching: ${error}`);
+    }
 
-const fanfic = fetchFanfic(fanficId)
-    .then((fanfic) => {
-        console.log(fanfic.title);
-        fanficNameInfo = fanfic.title;
-        console.log(fanficNameInfo);
-        authorInfo = fanfic.authorName;
-        ratingInfo = fanfic.ageLimit;
-        focusInfo = fanfic.category;
-        likesInfo = fanfic.numLikes;
-        // const sizeChaptersInfo = fanfic.numChapter;
-        charactersInfo = fanfic.characters;
-        pairingsInfo = fanfic.pairings;
-        genresInfo = fanfic.genre;
-        warningsInfo = fanfic.warnings;
-        descriptionInfo = fanfic.description;
-        notesInfo = fanfic.notes;
-        fandomInfo = fanfic.fandom;
+    const fanficNameInfo = fanfic.title;
+    const authorInfo = fanfic.authorName;
+    const ratingInfo = fanfic.ageLimit;
+    let focusInfo;
 
-console.log(fanfic);
-console.log(fetchChapter(fanficId, 1));
+    switch (fanfic.category) {
+    case 'Слэш':
+        focusInfo = '⚣';
+        break;
+    case 'Фемслэш':
+        focusInfo = '⚢';
+        break;
+    case 'Гет':
+        focusInfo = '⚤';
+        break;
+    case 'Джен':
+        focusInfo = '⚔︎';
+        break;
+    case 'Смешанная':
+        focusInfo = '〰';
+        break;
+    case 'Статья':
+        focusInfo = '✒';
+        break;
+    case 'Другое':
+        focusInfo = '◯';
+        break;
+    default:
+        focusInfo = '◯';
+    }
 
-const fanficName = document.getElementById('fanficName');
-const rating = document.getElementById('rating');
-const _status = document.getElementById('status');
-const _focus = document.getElementById('focus');
-const likes = document.getElementById('likes');
-const author = document.getElementById('author');
+    const likesInfo = fanfic.numLikes;
+    const sizeChaptersInfo = 2; //fanfic.numChapter;
+    const charactersInfo = fanfic.characters;
+    const pairingsInfo = fanfic.pairings;
+    const genresInfo = fanfic.genre;
+    const warningsInfo = fanfic.warnings;
+    const descriptionInfo = fanfic.description;
+    const notesInfo = fanfic.authorNotes;
+    const fandomInfo = fanfic.fandom;
 
-author.setAttribute('data-link', 'profile.html');
-author.setAttribute('style', 'cursor: pointer;')
-author.addEventListener('click', () => {
-    const link = author.getAttribute('data-link');
-    window.location.href = link;
-});
+    // console.log();
+    // console.log(fetchChapter(fanficId, 1));
 
-const fandom = document.getElementById('fandom');
-const notes = document.getElementById('notes');
-const sizeChapters = document.getElementById('sizeChapters');
-// const sizePages = document.getElementById('sizePages');
-const characters = document.getElementById('characters');
-const pairings = document.getElementById('pairings');
-const genres = document.getElementById('genres');
-const warnings = document.getElementById('warnings');
-const description = document.getElementById('description');
+    const fanficName = document.getElementById('fanficName');
+    const rating = document.getElementById('rating');
+    // const _status = document.getElementById('status');
+    const _focus = document.getElementById('focus');
+    const likes = document.getElementById('likes');
+    const author = document.getElementById('author');
 
-// const [fanficNameInfo, ratingInfo, statusInfo, focusInfo,
-//         likesInfo, authorInfo, sizeChaptersInfo, 
-//         sizePagesInfo, charactersInfo, pairingsInfo,
-//         genresInfo, warningsInfo, descriptionInfo] = getFanficInfo();
+    author.setAttribute('data-link',`/profile.html?username=${authorInfo}`);
+    author.setAttribute('style', 'cursor: pointer;')
+    author.addEventListener('click', () => {
+        const link = author.getAttribute('data-link');
+        window.location.href = link;
+    });
 
-loadingData(fanficNameInfo, fanficName);
-loadingData(ratingInfo, rating);
-// loadingData(statusInfo, _status);
-loadingData(focusInfo, _focus);
-loadingData(likesInfo, likes);
-loadingData(authorInfo, author);
-// loadingData(sizeChaptersInfo, sizeChapters);
-// loadingData(sizePagesInfo, sizePages);
-loadingData(charactersInfo, characters);
-loadingData(pairingsInfo, pairings);
-loadingData(genresInfo, genres);
-loadingData(warningsInfo, warnings);
-loadingData(descriptionInfo, description);
+    const fandom = document.getElementById('fandom');
+    const notes = document.getElementById('notes');
+    const sizeChapters = document.getElementById('sizeChapters');
+    const characters = document.getElementById('characters');
+    const pairings = document.getElementById('pairings');
+    const genres = document.getElementById('genres');
+    const warnings = document.getElementById('warnings');
+    const description = document.getElementById('description');
+    const chapterLabel = document.getElementById('chapterLabel');
 
-const nameChapter = document.getElementById('nameChapter');
-const contentChapter = document.getElementById('contentChapter');
-let currentChapter = parseInt(localStorage.getItem('currentChapter')) || 0;
+    loadingData(fanficNameInfo, fanficName);
+    loadingData(ratingInfo, rating);
+    // loadingData(statusInfo, _status);
+    loadingData(focusInfo, _focus);
+    loadingData(likesInfo, likes);
+    loadingData(authorInfo, author);
+    loadingData(sizeChaptersInfo, sizeChapters);
+    loadingData(charactersInfo, characters);
+    loadingData(pairingsInfo, pairings);
+    loadingData(genresInfo, genres);
+    loadingData(warningsInfo, warnings);
+    loadingData(descriptionInfo, description);
+    loadingData(fandomInfo, fandom);
+    loadingData(notesInfo, notes);
 
-let [nameChapterInfo, contentChapterInfo] = getChapterInfo(currentChapter);
+    if (sizeChaptersInfo % 10 === 1 && sizeChaptersInfo % 100 !== 11) {
+        chapterLabel.textContent = 'глав';
+    } else if (sizeChaptersInfo % 10 >= 2 && sizeChaptersInfo % 10 <= 4 
+        && (sizeChaptersInfo % 100 < 10 || sizeChaptersInfo % 100 >= 20)) {
+        chapterLabel.textContent = 'главы';
+    } else {
+        chapterLabel.textContent = 'глав';
+    }
 
-let textNodeName = document.createTextNode(nameChapterInfo);
-nameChapter.appendChild(textNodeName);
+    let chapter;
 
-let textNodeChapter = document.createTextNode(contentChapterInfo);
-contentChapter.appendChild(textNodeChapter);
+    const nameChapter = document.getElementById('nameChapter');
+    const contentChapter = document.getElementById('contentChapter');
+    let currentChapter = parseInt(localStorage.getItem('currentChapter')) || 1;
 
-const nextButton = document.querySelector('#nextChapter');
-const prevButton = document.querySelector('#prevChapter');
+    try {
+        chapter = await fetchChapter(fanficId, currentChapter);
+    } catch (error) {
+        console.error(`Error fetching: ${error}`);
+        console.error(fanficId, currentChapter);
+    }
 
-nextButton.addEventListener('click', () => {
-    currentChapter = Math.min(currentChapter + 1, parseInt(sizeChaptersInfo) - 1);
-    localStorage.setItem('currentChapter', currentChapter.toString());
-    let [NCI, CCI] = getChapterInfo(currentChapter);
-    changeChapter(NCI, CCI);
+    // let [nameChapterInfo, contentChapterInfo] = getChapterInfo(currentChapter);
+    let nameChapterInfo = chapter.title;
+    let contentChapterInfo = chapter.content;
 
-    prevButton.style.visibility = 'visible';
-    if(currentChapter === parseInt(sizeChaptersInfo) - 1) {
+    let textNodeName = document.createTextNode(nameChapterInfo);
+    nameChapter.appendChild(textNodeName);
+
+    let textNodeChapter = document.createTextNode(contentChapterInfo);
+    contentChapter.appendChild(textNodeChapter);
+
+    const nextButton = document.querySelector('#nextChapter');
+    const prevButton = document.querySelector('#prevChapter');
+
+    if(currentChapter === parseInt(sizeChaptersInfo)) {
         nextButton.style.visibility = 'hidden';
     }
 
-  });
-  
-prevButton.addEventListener('click', () => {
-    currentChapter = Math.max(currentChapter - 1, 0);
-    localStorage.setItem('currentChapter', currentChapter.toString());
-    let [NCI, CCI] = getChapterInfo(currentChapter);
-    changeChapter(NCI,CCI);
-
-    nextButton.style.visibility = 'visible';
-    if(currentChapter === 0) {
+    if(currentChapter === 1) {
         prevButton.style.visibility = 'hidden';
     }
-  });
 
-const user = 'SoftOwl256';
-const likeButton = document.querySelector('#likeBtn');
-let IsLike = getIsLike() || false;
+    nextButton.addEventListener('click', async () => {
+        currentChapter = Math.min(currentChapter + 1, parseInt(sizeChaptersInfo));
+        localStorage.setItem('currentChapter', currentChapter.toString());
+        try {
+            chapter = await fetchChapter(fanficId, currentChapter);
+        } catch (error) {
+            console.error(`Error fetching: ${error}`);
+            console.error(fanficId, currentChapter);
+        }
+        nameChapter.removeChild(textNodeName);
+        textNodeName = document.createTextNode(chapter.title);
+        nameChapter.appendChild(textNodeName);
+    
+        contentChapter.removeChild(textNodeChapter);
+        textNodeChapter = document.createTextNode(chapter.content);
+        contentChapter.appendChild(textNodeChapter);
 
-const addChapterBtn = document.getElementById('addChapter');
-const statusBtn = document.getElementById('changeStatus');
-
-if (authorInfo !== user) {
-    statusBtn.setAttribute('style', 'display: none;');
-    addChapterBtn.setAttribute('style', 'display: none;');
-
-    const imgLike = document.createElement('img');
-    imgLike.classList.add('button-star-icon');
-    imgLike.setAttribute('src', 'assets/images/star2.svg');
-    imgLike.setAttribute('alt', 'like');
-    likeButton.appendChild(imgLike);
-
-
-    if (IsLike) likeButton.style.backgroundColor = 'rgb(112, 36, 20, 0.3)';
-
-    likeButton.addEventListener('click', () => {
-        IsLike = !IsLike;
-        likeButton.style.backgroundColor = IsLike ? 'rgb(112, 36, 20, 0.3)' : 'white';
-        setLikeValue();
-    });
-} else {
-    likeButton.setAttribute('style', 'display: none;');
-
-    addChapterBtn.setAttribute('style', 'display: block;');
-    addChapterBtn.addEventListener('click', () => {
-        // window.location.href = `/add-chapter.html?fanficId=${fanficId}`;
-        window.location.href = `add-chapter.html`;
+        prevButton.style.visibility = 'visible';
+        if(currentChapter === parseInt(sizeChaptersInfo)) {
+            nextButton.style.visibility = 'hidden';
+        }
     });
 
-    statusBtn.setAttribute('style', 'display: block;');
-    const popup = document.getElementById('popup');
+    prevButton.addEventListener('click', async () => {
+        currentChapter = Math.max(currentChapter - 1, 1);
+        localStorage.setItem('currentChapter', currentChapter.toString());
+        try {
+            chapter = await fetchChapter(fanficId, currentChapter);
+        } catch (error) {
+            console.error(`Error fetching: ${error}`);
+            console.error(fanficId, currentChapter);
+        }
 
-    statusBtn.addEventListener('click', () => {
-    console.log('click!');
-    popup.style.display = 'flex';
+        nameChapter.removeChild(textNodeName);
+        textNodeName = document.createTextNode(chapter.title);
+        nameChapter.appendChild(textNodeName);
+    
+        contentChapter.removeChild(textNodeChapter);
+        textNodeChapter = document.createTextNode(chapter.content);
+        contentChapter.appendChild(textNodeChapter);
+
+        nextButton.style.visibility = 'visible';
+        if(currentChapter === 1) {
+            prevButton.style.visibility = 'hidden';
+        }
     });
 
-    popup.addEventListener('click', function(event) {
-    if (event.target.tagName === 'BUTTON') {
-        const selectedOption = event.target.textContent;
-        console.log('Выбран вариант:', selectedOption); // Засетить статус
-        popup.style.display = 'none';
+    const user = 'softowl256';
+    const likeButton = document.querySelector('#likeBtn');
+    let IsLike = getIsLike() || false;
+
+    const addChapterBtn = document.getElementById('addChapter');
+    // const statusBtn = document.getElementById('changeStatus');
+
+    if (authorInfo !== user) {
+        // statusBtn.setAttribute('style', 'display: none;');
+        addChapterBtn.setAttribute('style', 'display: none;');
+
+        const imgLike = document.createElement('img');
+        imgLike.classList.add('button-star-icon');
+        imgLike.setAttribute('src', 'assets/images/star2.svg');
+        imgLike.setAttribute('alt', 'like');
+        likeButton.appendChild(imgLike);
+
+
+        if (IsLike) likeButton.style.backgroundColor = 'rgb(112, 36, 20, 0.3)';
+
+        likeButton.addEventListener('click', () => {
+            IsLike = !IsLike;
+            likeButton.style.backgroundColor = IsLike ? 'rgb(112, 36, 20, 0.3)' : 'white';
+            setLikeValue(IsLike);
+        });
+    } else {
+        likeButton.setAttribute('style', 'display: none;');
+
+        addChapterBtn.setAttribute('style', 'display: block;');
+        addChapterBtn.addEventListener('click', () => {
+            window.location.href = `/add-chapter.html?fanficId=${fanficId}`;
+            // window.location.href = `add-chapter.html`;
+        });
+
+        // statusBtn.setAttribute('style', 'display: block;');
+        // const popup = document.getElementById('popup');
+
+        // statusBtn.addEventListener('click', () => {
+        // console.log('click!');
+        // popup.style.display = 'flex';
+        // });
+
+        // popup.addEventListener('click', function(event) {
+        // if (event.target.tagName === 'BUTTON') {
+        //     const selectedOption = event.target.textContent;
+        //     console.log('Выбран вариант:', selectedOption); // Засетить статус
+        //     popup.style.display = 'none';
+        // }
+        // });
+
+        // document.addEventListener('click', function(event) {
+        // if (!popup.contains(event.target) && event.target !== statusBtn) {
+        // popup.style.display = 'none';
+        // }
+        // });
     }
+
+    const openModalButton = document.getElementById('gallery');
+    openModalButton.setAttribute('style', 'cursor: pointer;');
+    const modal = document.getElementById('modal');
+    const closeButton = modal.querySelector('.close');
+
+    openModalButton.addEventListener('click', () => {
+    modal.style.display = 'block';
     });
 
-    document.addEventListener('click', function(event) {
-    if (!popup.contains(event.target) && event.target !== statusBtn) {
-    popup.style.display = 'none';
+    closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+    });
+
+    const imageContainer = document.getElementById('image-container');
+    const images = getImages();
+
+    for(let i = 0; i < images.length; i++) {
+        const image = document.createElement('img');
+
+        image.src = images[i];
+        image.alt = 'illustration';
+
+        image.style.maxWidth = '30rem';
+        image.style.maxHeight = '30rem';
+
+        imageContainer.appendChild(image);
     }
-    });
 }
-
-const openModalButton = document.getElementById('gallery');
-openModalButton.setAttribute('style', 'cursor: pointer;');
-const modal = document.getElementById('modal');
-const closeButton = modal.querySelector('.close');
-
-openModalButton.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-closeButton.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-const imageContainer = document.getElementById('image-container');
-const images = getImages();
-
-for(let i = 0; i < images.length; i++) {
-    const image = document.createElement('img');
-
-    image.src = images[i];
-    image.alt = 'illustration';
-
-    image.style.maxWidth = '30rem';
-    image.style.maxHeight = '30rem';
-
-    imageContainer.appendChild(image);
-}
-
-});
 
 function getIsLike() {
     return localStorage.getItem('like') || false;
 }
 
-function setLikeValue() {
+function setLikeValue(IsLike) {
     if (IsLike) {
         alert('Вы лайкнули работу!')
         localStorage.setItem('like', true);
@@ -219,7 +282,7 @@ function setLikeValue() {
     }
 }
 
-function changeChapter(NCI, CCI) {
+function changeChapter(NCI, CCI, textNodeName, textNodeChapter) {
     nameChapter.removeChild(textNodeName);
     textNodeName = document.createTextNode(NCI);
     nameChapter.appendChild(textNodeName);
@@ -242,7 +305,6 @@ function getChapterInfo(curr) {
 }
 
 function loadingData(base, id) {
-    console.log(base);
     const textNode = document.createTextNode(base);
     id.appendChild(textNode);
 }
