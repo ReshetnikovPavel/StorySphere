72866436890f325e84d94cbb23a47faf3fafc4e0
AndreyKuklinov <Andreydolphin@mail.ru>
2023-06-24 20:08:14 +0500

    Fix registration

--- FanfictionBackend/FanfictionBackend/wwwroot/assets/js/registration.js
+++ FanfictionBackend/FanfictionBackend/wwwroot/assets/js/registration.js
@@ -6,15 +6,27 @@ const password = document.getElementById('passwordRegistration');
 const confirmPassword = document.getElementById('confirm-password');
 const username = document.getElementById('name');
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault();
 
     const data = {
         email: email.value,
         username: username.value
     };
-    registerUser(data, password.value)
-        .catch(() => alert("Не удалось зарегистрироваться"));
+    const ok = await registerUser(data, password.value);
+
+    if(ok) {
+        login(email.value, password.value);
+    }
+}
+
+async function login(email, password) {
+    console.log(email);
+    console.log(password);
+    userData = await fetchSession(email, password);
+    Cookies.set('sessionToken', userData.token, {sameSite: 'strict'});
+    Cookies.set('username', userData.user.username, {sameSite: 'strict'});
+    window.location.href = `/profile.html?username=${userData.user.username}`;
 }
 
 function validatePassword() {
@@ -33,5 +45,22 @@ async function registerUser(data, password) {
         },
         body: JSON.stringify(data)
     });
-    return await response.json();
+    
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+    return response.ok;
+}
+
+async function fetchSession(email, password) {
+    const url = `/session?email=${email}&password=${password}`;
+    const response = await fetch(url);
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+    return data;
 }
