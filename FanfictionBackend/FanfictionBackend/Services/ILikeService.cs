6080ceaa5c05653baedcc49f63217f7bc18fbbef
AndreyKuklinov <Andreydolphin@mail.ru>
2023-06-24 13:35:50 +0500

    Add ILikeService

---
+++ FanfictionBackend/FanfictionBackend/Services/ILikeService.cs
@@ -0,0 +1,7 @@
+﻿namespace FanfictionBackend.Services;
+
+public interface ILikeService
+{
+    IResult AddLike(int fanficId, string userName);
+    IResult RemoveLike(int fanficId, string authorName);
+}
